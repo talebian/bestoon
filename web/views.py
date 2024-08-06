6197657d68be571d9eeaa96b7d4cba4e0b539327
alireza <alirezatalebian64@gmail.com>
2024-08-06 17:59:41 +0330

    submit_expense modified: it works with GET method beside Postman app

--- web/views.py
+++ web/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from json import JSONEncoder
+from web.models import User, Token, Expense, Income
+import datetime
 # Create your views here.
 
 def submit_expense(request):
-    """user submits an expense"""
-    print ('we are here: 4+40 = 00045000')
+    """ submit an expense """
+
+    #TODO: validate data, user might be fake, token might be fake, amount might be...
+    this_token = request.GET['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.GET:
+        date = datetime.datetime.now() 
+    Expense.objects.create(user = this_user, amount = request.GET['amount'],
+                           text = request.GET['text'], date = date)
+
+    print("I'm in submit expense with GET method")
+    print(request.GET)
+
+    return JsonResponse({
+        'status': 'ok > GET method'
+    }, encoder=JSONEncoder)
